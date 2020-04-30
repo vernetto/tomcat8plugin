75997e700c38155dd90977bc87c6b0af9acd4cce
vernetto <publicpierre@gmail.com>
2020-04-30 16:30:14 +0200

    first

---
+++ src/main/java/org/pierre/tomcat8plugin/Tomcat8pluginApplication.java
@@ -0,0 +1,13 @@
+package org.pierre.tomcat8plugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tomcat8pluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tomcat8pluginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pierre/tomcat8plugin/Tomcat8pluginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pierre.tomcat8plugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tomcat8pluginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
